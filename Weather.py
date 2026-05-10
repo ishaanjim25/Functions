da31ea67359bd6bdc046560975c8ace76f3117c8
ishaanjim25 <ishaanjim25@gmail.com>
2026-05-10 16:36:24 +0100

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,8 @@
+def weather():
+    print("Weather is pleasnt in ",spring)
+    print("Weather is the same in ",autmn)
+
+spring="autmn"
+autmn=spring
+
+weather()
---
+++ Well.py
@@ -0,0 +1,4 @@
+def wishes():
+    print("Hello\nGood luck to you")
+
+wishes()
